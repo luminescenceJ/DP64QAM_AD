0ecd30098065fd1d42a477a5c1b6f37e92fdf0b2
lumin <Jia1826240406@gmail.com>
2024-10-30 12:28:21 +0800

    10-30

--- models/LSTM_recon.py
+++ models/LSTM_recon.py
@@ -4,21 +4,32 @@ import torch.nn.functional as F
 import math
 
 class Model(nn.Module):
-    def __init__(self, args, input_dim=12, hidden_dim=128, num_layers=4, dropout=0.2, seq_len=1366, num_classes=9):
+    def __init__(self, args, input_dim=1366, hidden_dim=256, num_layers=4, dropout=0.2, seq_len=12,pred_len=1):
         super(Model, self).__init__()
         self.args = args
+        self.seq_len = seq_len
+        self.input_dim = input_dim
+        self.pred_len = pred_len
 
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         # LSTM层
-        self.lstm_time = nn.LSTM(
-            input_size=input_dim, hidden_size=hidden_dim * 2, num_layers=num_layers, dropout=dropout, batch_first=True
+        self.lstm_time_encoder = nn.LSTM(
+            input_size=input_dim, hidden_size=hidden_dim, num_layers=num_layers, dropout=dropout, batch_first=True
         )
-        self.lstm_freq = nn.LSTM(
+        self.lstm_freq_encoder = nn.LSTM(
+            input_size=input_dim, hidden_size=hidden_dim, num_layers=num_layers, dropout=dropout, batch_first=True
+        )
+
+        self.lstm_time_decoder = nn.LSTM(
+            input_size=input_dim, hidden_size=hidden_dim, num_layers=num_layers, dropout=dropout, batch_first=True
+        )
+        self.lstm_freq_decoder = nn.LSTM(
             input_size=input_dim, hidden_size=hidden_dim, num_layers=num_layers, dropout=dropout, batch_first=True
         )
 
         # 全连接层
-        self.fc1_time = nn.Linear(hidden_dim * 2, 12)
-        self.fc1_freq = nn.Linear(hidden_dim, 12)
+        self.fc_time = nn.Linear(hidden_dim, self.input_dim)
+        self.fc_freq = nn.Linear(hidden_dim, self.input_dim)
 
         # 激活函数
         self.relu = nn.ReLU()
@@ -34,19 +45,52 @@ class Model(nn.Module):
         freq_pad = F.pad(freq, (0, padding_needed), "constant", 0).unsqueeze(2).reshape(self.args.batch_size, -1,
                                                                                         self.args.reduce_ch)
 
+
         # LSTM 编码器
-        encoded_time, (hn_time, cn_time) = self.lstm_time(time_pad)  # LSTM输出和隐状态
-        encoded_freq, (hn_freq, cn_freq) = self.lstm_freq(freq_pad)  # BS,1366,128
+        _, decoder_hidden_time = self.lstm_time_encoder(time_pad.permute(0,2,1))  # LSTM输出和隐状态
+        _, decoder_hidden_freq = self.lstm_freq_encoder(freq_pad.permute(0,2,1))  # BS,1366,128
 
-        # 全连接层处理 - 频域
-        out_freq = self.fc1_freq(encoded_freq)  # bs,1366,12
-        out_freq = self.relu(out_freq).reshape(batch_size, -1)[:, :16384]
+        decoder_input_time = torch.zeros(batch_size, self.pred_len, self.input_dim).to(self.device)
+        outputs_time = torch.zeros(self.seq_len, batch_size, self.input_dim).to(self.device)
 
-        # 全连接层处理 - 时域
-        out_time = self.fc1_time(encoded_time)
-        out_time = self.relu(out_time).reshape(batch_size, -1)[:, :16384]
+        decoder_input_freq = torch.zeros(batch_size, self.pred_len, self.input_dim).to(self.device)
+        outputs_freq = torch.zeros(self.seq_len, batch_size, self.input_dim).to(self.device)
 
-        # 组合时域和频域的输出
-        out = torch.cat((out_time.reshape(batch_size, 16384, 1), out_freq.reshape(batch_size, 16384, 1)),
-                        dim=-1)  # bs,9,512
-        return out.reshape(batch_size, 16384, 2)
+        for t in range(self.seq_len):
+            decoder_output_time, decoder_hidden_time = self.lstm_time_encoder(decoder_input_time, decoder_hidden_time)
+            decoder_output_time = self.relu(decoder_output_time)
+            decoder_input_time = self.fc_time(decoder_output_time)
+            outputs_time[t] = torch.squeeze(decoder_input_time, dim=-2)
+
+            decoder_output_freq, decoder_hidden_time = self.lstm_time_encoder(decoder_input_freq, decoder_hidden_freq)
+            decoder_output_freq = self.relu(decoder_output_freq)
+            decoder_input_freq = self.fc_time(decoder_output_freq)
+            outputs_freq[t] = torch.squeeze(decoder_input_freq, dim=-2)
+
+        outputs_freq = outputs_freq.permute(1,0,2).reshape(batch_size, -1,1)[:, :16384,:]
+        outputs_time = outputs_time.permute(1,0,2).reshape(batch_size, -1,1)[:, :16384,:]
+
+        outputs = torch.concatenate((outputs_time,outputs_freq),dim=-1)
+        return outputs
+
+
+
+
+class args():
+    pass
+args.modulation = 12
+args.reduce_ch = 12
+args.seq_len = 16384
+args.batch_size = 8
+model = Model(args)
+
+input_data = torch.randn(8, 16384, 2)
+output_data = model(input_data)
+
+from torchinfo import summary
+
+summary(model,(8,16384,2))
+
+# 输出结果形状
+print("Input shape:", input_data.shape)
+print("Output shape:", output_data.shape)
